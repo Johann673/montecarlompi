55ea4552b98d148f826f7aa472e225e9cfc7d926
CaptainKali <clement.defigueiredo@gmail.com>
2014-04-15 16:33:22 -0400

    Cleaning header.h

--- header.h
+++ header.h
@@ -1,13 +1,13 @@
-
+// Versions de l'implémentation
 double play_seq(int repetition, int affiche);
 double play_statique(int repetition, int affiche);
 double play_dynamique(int repetition, int affiche);
 
+// Fonction auxililaires
 int nbElementsParTache(int position, int nbTachesTotales, int nbThreads);
 int find_index(int a[], int num_elements, int value);
+
+// Opérations appelées dans les différentes versions du programme
 void tirage(int combinaison[], int resultat[], int nbElements, int numProc);
 void coordonnateur(int* resultatFinal, int repetition, int nbParTache, int nbTravailleurs);
 void travailleur(int* combinaison, MPI_Comm comm);
-
-
-
